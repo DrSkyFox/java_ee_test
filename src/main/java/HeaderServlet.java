9496e34bf253fb3c3f9665fc8d882d42c9568206
DrSkyFox <52832487+DrSkyFox@users.noreply.github.com>
2020-11-18 01:14:13 +0300

    init

--- src/main/java/HeaderServlet.java
+++ src/main/java/HeaderServlet.java
@@ -5,15 +5,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "menu",urlPatterns = "/menu")
+@WebServlet("/page_header")
 public class HeaderServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        getServletContext().getRequestDispatcher("").forward(req, resp);
-    }
+        resp.getWriter().println("<link rel='stylesheet' href='" + req.getContextPath() + "/style.css'>");
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String header = (String) req.getAttribute("header");
 
+        resp.getWriter().println("<h1>" + header + "</h1>");
+        resp.getWriter().println("<ul>");
+        resp.getWriter().println("<li><a href='"+ req.getContextPath() + "/first_http_servlet'>Home</a></li>");
+        resp.getWriter().println("<li><a href='#news'>News</a></li>");
+        resp.getWriter().println("<li><a href='#contacts'>Contacts</a></li>");
+        resp.getWriter().println("<li><a href='#about'>About</a></li>");
+        resp.getWriter().println("</ul>");
     }
 }
